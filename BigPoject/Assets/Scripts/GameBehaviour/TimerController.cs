012f34fbbd7ec082ede2fdba8159e8fddcf97bfc
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-06-05 11:41:28 +0300

    Done with TimerController class.

--- BigPoject/Assets/Scripts/GameBehaviour/TimerController.cs
+++ BigPoject/Assets/Scripts/GameBehaviour/TimerController.cs
@@ -1,4 +1,31 @@
-﻿public class TimerController
+﻿using UnityEngine;
+
+public static class TimerController
 {
-    
+    // Decrement timer by Time.deltaTime value.
+    public static void DecrementByDeltaTime(ref float timer)
+    {
+        timer -= Time.deltaTime;
+    }
+
+
+    // Increment timer by Time.deltaTime value.
+    public static void IncrementByTimeDeltaTime(ref float timer)
+    {
+        timer += Time.deltaTime;
+    }
+
+
+    // Set timer to zero.
+    public static void SetToZero(ref float timer)
+    {
+        timer = 0f;
+    }
+
+
+    // Set timer to user value.
+    public static void SetToValue(ref float timer, float valueToSet)
+    {
+        timer = valueToSet;
+    }
 }
