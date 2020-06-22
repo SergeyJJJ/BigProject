8b80c5e5227c5598825b16fcf5374a7f2b3030e3
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-06-22 09:37:46 +0300

    Created bullet pool. Add minor improvements.

---
+++ BigPoject/Assets/Scripts/Arsenal/Bullets/BulletPool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Arsenal.Bullets
+{
+    public class BulletPool : MonoBehaviour
+    {
+        [SerializeField] private GameObject _bullet = null;
+        [SerializeField] private int _bulletsAmount = 0;
+        private List<GameObject> _bulletsPool;
+        public static BulletPool SharedInstance;
+        
+        private void Awake()
+        {
+            SharedInstance = this;
+        }
+
+    
+        private void Start()
+        {
+            // Filling pool with bullets.
+            _bulletsPool = new List<GameObject>();
+            for (int i = 0; i < _bulletsAmount; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(_bullet);
+                obj.SetActive(false);
+                _bulletsPool.Add(obj);
+                obj.transform.SetParent(this.transform);
+            }
+        }
+
+    
+        // Get bullet if it is available.
+        public GameObject GetPooledObject()
+        {
+            for (int i = 0; i < _bulletsPool.Count; i++)
+            {
+                if (!_bulletsPool[i].activeInHierarchy)
+                {
+                    return _bulletsPool[i];
+                }
+            }
+        
+            return null;
+        }
+    }
+}
--- BigPoject/Assets/Scripts/Arsenal/Weapons/AutoRifle.cs
+++ BigPoject/Assets/Scripts/Arsenal/Weapons/AutoRifle.cs
@@ -5,6 +5,20 @@ namespace Assets.Scripts.Arsenal.Weapons
     [RequireComponent((typeof(SpriteRenderer)))]
     public class AutoRifle : Weapon
     {
+        private SpriteRenderer _spriteRenderer = null;
+        
+        private void Awake()
+        {
+            _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        }
+
+
+        private void Start()
+        {
+            _spriteRenderer.sprite = InGameSprite;
+        }
+        
+        
         public override void Shoot()
         {
             throw new System.NotImplementedException();
