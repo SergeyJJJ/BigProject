ad35262e1955df0f3bdc914bfef4c19ddd790889
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-07-16 11:16:35 +0300

    Started to create AStar pathfinding system.

---
+++ BigPoject/Assets/Scripts/AStar/Grid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] private LayerMask _whatIsPassable = Physics2D.AllLayers;
+    [SerializeField] private Vector2 _gridSizeInWorld = Vector2.zero;
+    [SerializeField] private float nodeRadius;
+    private Node[,] _grid = null;
+
+    #region Properties
+
+    public Vector2 GridSizeInWorld => _gridSizeInWorld;
+
+    public float NodeRadius => nodeRadius;
+
+    #endregion Properties
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, _gridSizeInWorld);
+    }
+}
---
+++ BigPoject/Assets/Scripts/AStar/Node.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class Node
+{
+    private bool _isWalkable;                      // Determine if current node is walkable.
+    private Vector2 _positionInWorld;              // Determine where is node placed in world space.
+
+    #region Properties
+
+    public bool IsWalkable => _isWalkable;
+
+    public Vector2 PositionInWorld
+    {
+        get => _positionInWorld;
+        set => _positionInWorld = value;
+    }
+
+    #endregion Properties
+
+    public Node(bool isWalkable, Vector2 positionInWorld)
+    {
+        _isWalkable = isWalkable;
+        _positionInWorld = positionInWorld;
+    }
+}
