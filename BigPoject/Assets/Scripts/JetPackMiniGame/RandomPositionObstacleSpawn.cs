c8d911263f2f7ce06396689923db2d1e0511d9d5
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-08-01 09:52:10 +0300

    Improved RandomPositionObstacleSpawn.cs. Added Gates obstacle.

--- BigPoject/Assets/Scripts/JetPackMiniGame/RandomPositionObstacleSpawn.cs
+++ BigPoject/Assets/Scripts/JetPackMiniGame/RandomPositionObstacleSpawn.cs
@@ -6,18 +6,19 @@ namespace JetPackMiniGame
 {
     public class RandomPositionObstacleSpawn : MonoBehaviour
     {
-        [SerializeField] private float _workingTime = 0f;                // Determine how long obstacles will be spawned.
-        [SerializeField] private float _timeBetweenEachObstacle = 0f;    // Time that must pass before a new obstacle will be spawned.
-        private float _newObstacleSpawnTimer = 0f;                       // Timer that controls time between obstacles spawn.
-        
-        [Serializable] private class SpawnBoundaries                    // Struct that contains obstacle spawn boundaries.
+        [Serializable] private class HorizontalSpawnBoundaries                                // Struct that contains obstacle spawn boundaries on X-Axis.
         {
             public int leftBoundary = 0;
             public int rightBoundary = 0;
-            public int downBoundary = 0;
-            public int upBoundary = 0;
         }
-        [SerializeField] private SpawnBoundaries _spawnConstraints = null;
+        
+        [SerializeField] private float _workingTime = 0f;                                     // Determine how long obstacles will be spawned.
+        [SerializeField] private float _timeBetweenEachObstacle = 0f;                         // Time that must pass before a new obstacle will be spawned.
+        private float _newObstacleSpawnTimer = 0f;                                            // Timer that controls time between obstacles spawn.
+        
+        [Space]
+        [SerializeField] private int _verticalSpawnPosition = 0;                              // Position on Y-Axis where obstacles will be spawned.
+        [SerializeField] private HorizontalSpawnBoundaries _horizontalSpawnBoundaries = null; // Instance horizontal spawn boundaries data type, used to get access to individual fields.
 
         public void SpawnObstacles(float workingTime)
         {
@@ -45,35 +46,18 @@ namespace JetPackMiniGame
                     _newObstacleSpawnTimer = _timeBetweenEachObstacle;
 
                     GameObject obstacle = ObstaclePool.SharedInstance.GetPooledObstacle();
-                    AdjacentObstaclesCheck adjacentObstaclesCheck = obstacle.GetComponent<AdjacentObstaclesCheck>();
 
                     // If obstacle was received from the pool.
                     if (obstacle != null)
                     {
-                        // Do, while the obstacle will be placed to the position
-                        // that doesn`t instersects with another obstacles. Or while
-                        // attempts of finding clear position is not run out.
-                        int positionFindingAttemptsCounter = 0;
-                        const int positionSearchAttemptsLimit = 20;
-                        do
-                        {
-                            // Get random position to place an obstacle.
-                            Vector2 spawnPosition = GetRandomSpawnPosition();
+                        // Get random position to place an obstacle.
+                        Vector2 spawnPosition = GetRandomSpawnPosition();
 
-                            // Place received obstacle on received position.
-                            obstacle.transform.position = spawnPosition;
+                        // Place received obstacle on received position.
+                        obstacle.transform.position = spawnPosition;
 
-                            // Activate obstacle.
-                            obstacle.SetActive(true);
-
-                            // Increase position finding attempts counter.
-                            positionFindingAttemptsCounter++;
-
-                            // DEVELOPMENT PROCESS!!!
-                            break;
-
-                        } while ((adjacentObstaclesCheck.IsIntersectAnotherObstacle == true) &&
-                                 (positionFindingAttemptsCounter <= positionSearchAttemptsLimit));
+                        // Activate obstacle.
+                        obstacle.SetActive(true);
                     }
                 }
             }
@@ -84,10 +68,10 @@ namespace JetPackMiniGame
         {
             Random random = new Random();
             
-            float xRandomSpawnPosition = random.Next(_spawnConstraints.leftBoundary, _spawnConstraints.rightBoundary + 1);
-            float yRandomSpawnPosition = random.Next(_spawnConstraints.downBoundary, _spawnConstraints.upBoundary + 1);
+            float xRandomSpawnPosition = random.Next(_horizontalSpawnBoundaries.leftBoundary, _horizontalSpawnBoundaries.rightBoundary + 1);
+            float ySpawnPosition = _verticalSpawnPosition;
     
-            return new Vector2(xRandomSpawnPosition, yRandomSpawnPosition);
+            return new Vector2(xRandomSpawnPosition, ySpawnPosition);
         }
     }
 }
