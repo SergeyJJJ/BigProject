d0c68927406ecd1879df9a0d0c412f00a5cf0428
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-08-04 11:49:12 +0300

    Created DoubleGates obstacle and pool for that obstacles.

--- BigPoject/Assets/Scripts/JetPackMiniGame/MiniGameScenario.cs
+++ BigPoject/Assets/Scripts/JetPackMiniGame/MiniGameScenario.cs
@@ -1,29 +1,23 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace JetPackMiniGame
 {
     public class MiniGameScenario : MonoBehaviour
     {
-        [Header("Stages execution periods")]
-        [SerializeField] private float[] _executionPeriods = null;                                   // Define until what time specific spawn templates will be performed: seconds.
+        [Header("Pools of obstacles")]
+        [SerializeField] private ObstaclePool _singlePlatformsPool = null;
+        [SerializeField] private ObstaclePool _movingSinglePlatformsPool = null;
+        [SerializeField] private ObstaclePool _singleGatesPool = null;
+        [SerializeField] private ObstaclePool _doubleGatesPool = null;
 
-        [Space, Header("Obstacles pools")]
-        [SerializeField] private ObstaclePool _singlePlatformPool = null;                            // Contains pool of single platform obstacles.
-        [SerializeField] private ObstaclePool _gatesPool = null;                                     // Contains pool of gates obstacles.
-        [SerializeField] private ObstaclePool _movingSinglePlatformPool = null;                      // Contains pool of moving single platform obstacles.
-        
-        private RandomPositionObstacleSpawn _singlePlatformSpawn = null;                             // Contains reference to the specific spawn template.
-        private RandomPositionObstacleSpawn _gatesSpawn = null;                                      // Contains reference to the specific spawn template.      
-        private RandomPositionObstacleSpawn _movingSinglePlatformSpawn = null;                             // Contains reference to the specific spawn template.
-        
-        private float _executingTime = 0;                                                            // Time that controls what patterns will be performed at certain period of time.
+        private delegate void SpawnObstacleDelegate();              // Delegate that can contain obstacle spawn method.
+        private SpawnObstacleDelegate[] _obstacleSpawnTypes = { };  // Array contains collection of different obstacle spawn methods.
 
-        private void Awake()
+
+        private void SpawnSinglePlatform()
         {
-            _singlePlatformSpawn = new RandomPositionObstacleSpawn(_singlePlatformPool);
-            _gatesSpawn = new RandomPositionObstacleSpawn(_gatesPool);
-            _movingSinglePlatformSpawn = new RandomPositionObstacleSpawn(_movingSinglePlatformPool);
+            
         }
     }
 }
