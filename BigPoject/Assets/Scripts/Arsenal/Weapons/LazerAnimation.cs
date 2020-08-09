b0fbdbaf820680f3a40b51aa4dbb9bfc661f1e36
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-08-09 09:10:27 +0300

    Fixed lazer obstacle and created lazer part synchronize system.

---
+++ BigPoject/Assets/Scripts/Arsenal/Weapons/LazerAnimation.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Arsenal.Weapons
+{
+    public class LazerAnimation : MonoBehaviour
+    {
+        [SerializeField] private Animator _lazerStartAnimator = null;                // Animator component of the lazer start part.
+        [SerializeField] private Animator _lazerMiddleAnimator = null;               // Animator component of the lazer middle part.
+        [SerializeField] private Animator _lazerEndAnimator = null;                  // Animator component of the lazer end part.
+
+        private void OnEnable()
+        {
+            EventSystem.EventSystem.StartListening("OnLazerPartActive", SynchronizeLazerParts);
+        }
+    
+    
+        private void SynchronizeLazerParts()
+        {
+            if (_lazerStartAnimator.gameObject.activeInHierarchy)
+            {
+                _lazerStartAnimator.Play(0, -1, 0);
+            }
+
+            if (_lazerMiddleAnimator.gameObject.activeInHierarchy)
+            {
+                _lazerMiddleAnimator.Play(0, -1, 0);
+            }
+
+            if (_lazerEndAnimator.gameObject.activeInHierarchy)
+            {
+                _lazerEndAnimator.Play(0, -1, 0);
+            }
+        }
+
+
+        private void OnDisable()
+        {
+            EventSystem.EventSystem.StopListening("OnLazerPartActive", SynchronizeLazerParts);
+        }
+    }
+}
---
+++ BigPoject/Assets/Scripts/Arsenal/Weapons/LazerPart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Arsenal.Weapons
+{
+    public class LazerPart : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            EventSystem.EventSystem.TriggerEvent("OnLazerPartActive");
+        }
+    }
+}
