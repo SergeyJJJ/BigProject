817c8a1360bc7a20a5c901afb2c1e48d0029f195
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-09-01 11:00:07 +0300

    Created and applied WeaponCasingsParticles.cs. Also adjusted rifle`s casing particles settings.

---
+++ BigPoject/Assets/Scripts/Arsenal/Weapons/WeaponsCasingsParticles.cs
@@ -0,0 +1,31 @@
+﻿using GameBehaviour;
+using UnityEngine;
+
+namespace Arsenal.Weapons
+{
+    public class WeaponsCasingsParticles : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem _rifleCasings = null;             // Particles that used to represent AutoRifle casings.
+
+
+        private void OnEnable()
+        {
+            EventSystem.StartListening("OnRifleShot", ThrowRifleCasings);
+        }
+
+
+        private void ThrowRifleCasings()
+        {
+            if (_rifleCasings != null)
+            {
+                _rifleCasings.Play();
+            }
+        }
+
+
+        private void OnDisable()
+        {
+            EventSystem.StopListening("OnRifleShot", ThrowRifleCasings);
+        }
+    }
+}
