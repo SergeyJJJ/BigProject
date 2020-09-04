085b6fce2c246ade498c3fc68eb73a7823899aaa
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-09-04 17:43:56 +0300

    Now Collector script does not use layers to detect if object derived from ICollectable interface.

--- BigPoject/Assets/Scripts/Living beings/Player/Collector.cs
+++ BigPoject/Assets/Scripts/Living beings/Player/Collector.cs
@@ -6,23 +6,12 @@ namespace Living_beings.Player
 {
     public class Collector : MonoBehaviour
     {
-        [SerializeField] private LayerMask _collectable = Physics2D.AllLayers;              // Layers that determine which objects can be collected. 
-        
         private void OnTriggerEnter2D(Collider2D other)
         {
-            // If object is derive form ICollectable interface
+            // If object derive form ICollectable interface
             // use its Collect() method.
-            if (IsObjectCollectable(other.gameObject))
-            {
-                ICollectable collectableObject = other.gameObject.GetComponent<ICollectable>();
-                collectableObject?.Collect();
-            }
-        }
-
-
-        private bool IsObjectCollectable(GameObject collidedObject)
-        {
-            return ((1 << collidedObject.gameObject.layer) & _collectable) != 0;
+            ICollectable collectableObject = other.gameObject.GetComponent<ICollectable>();
+            collectableObject?.Collect();
         }
     }
 }
