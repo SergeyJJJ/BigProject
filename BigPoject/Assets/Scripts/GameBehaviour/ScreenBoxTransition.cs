08512096bf4da1d0863e3cc213f1dcb21f0fa477
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-09-15 10:23:33 +0300

    Now ScreenBox move player to proper point not only by x-coordinate, but in all other coordinates.

--- BigPoject/Assets/Scripts/GameBehaviour/ScreenBoxTransition.cs
+++ BigPoject/Assets/Scripts/GameBehaviour/ScreenBoxTransition.cs
@@ -9,15 +9,11 @@ namespace GameBehaviour
         [SerializeField] private GameObject _secondFrame = null;           // Second frame from which we can transit to another frames.
         [SerializeField] private Transform _leftPoint = null;              // Point in which direction  will be moved character if he will go to the left screenBox.
         [SerializeField] private Transform _rightPoint = null;             // Point to which direction will be moved character if he will go to the left screenBox.
-        private float _leftPointXCoordinate = 0;
-        private float _rightPointXCoordinate = 0;
         private CharacterMovement _characterMovement = null;               // Characters movement control script.
 
 
         private void Start()
         {
-            _leftPointXCoordinate = _leftPoint.position.x;
-            _rightPointXCoordinate = _rightPoint.position.x;
             _characterMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterMovement>();
         }
     
@@ -32,13 +28,13 @@ namespace GameBehaviour
                 {
                     _firstFrame.SetActive(false);
                     _secondFrame.SetActive(true);
-                    characterNextFramePosition = new Vector2(_rightPointXCoordinate, _characterMovement.transform.position.y);
+                    characterNextFramePosition = _rightPoint.position;
                 }
                 else if (_secondFrame.activeInHierarchy)
                 {
                     _secondFrame.SetActive(false);
                     _firstFrame.SetActive(true);
-                    characterNextFramePosition = new Vector2(_leftPointXCoordinate, _characterMovement.transform.position.y);
+                    characterNextFramePosition = _leftPoint.position;
                 }
 
                 if (characterNextFramePosition != Vector2.zero)
