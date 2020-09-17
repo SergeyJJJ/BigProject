68b2f8abbc83657900211e90c5570dec42cc8e13
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-09-17 13:37:55 +0300

    Add comments to the LootSpreader.cs and change name of private function form RandomSign to GetRandomSign.

--- BigPoject/Assets/Scripts/Environment/LootSpreader.cs
+++ BigPoject/Assets/Scripts/Environment/LootSpreader.cs
@@ -17,6 +17,9 @@ namespace Environment
 
         public void SpreadLoot()
         {
+            // Pass through every element in Loot array, and instantiate every
+            // item some amount of times. After item was instantiated apply 
+            // force to it to throw it on some distance.
             for (int lootIndex = 0; lootIndex < _loot.Length; lootIndex++)
             {
                 int itemAmount = _loot[lootIndex].amount;
@@ -42,13 +45,13 @@ namespace Environment
             int xMin = 5, xMax = 11;
             int yMin = 4, yMax = 8;
             
-            int xDirection = UnityEngine.Random.Range(xMin, xMax) * RandomSign();
-            int yDirection = UnityEngine.Random.Range(yMin, yMax) * RandomSign();
+            int xDirection = UnityEngine.Random.Range(xMin, xMax) * GetRandomSign();
+            int yDirection = UnityEngine.Random.Range(yMin, yMax) * GetRandomSign();
 
             return new Vector2(xDirection, yDirection).normalized;
         }
 
-        private int RandomSign()
+        private int GetRandomSign()
         {
             return UnityEngine.Random.Range(1, 3) == 1 ? -1 : 1;
         }
