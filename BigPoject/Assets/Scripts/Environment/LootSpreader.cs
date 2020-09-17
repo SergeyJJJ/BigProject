ff3e5aa38698f5737b6d418feabd2fb8292521c7
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-09-17 13:33:45 +0300

    Fixed index out of range exception in LootSpreader.cs.

--- BigPoject/Assets/Scripts/Environment/LootSpreader.cs
+++ BigPoject/Assets/Scripts/Environment/LootSpreader.cs
@@ -1,41 +1,56 @@
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 namespace Environment
 {
     public class LootSpreader : MonoBehaviour
     {
-        [SerializeField] private List<GameObject> _lootStorage = null;             // Storage for loot that must be spread.
+        [Serializable] public struct Loot                                         // Used as storage for different loot that will be thrown.
+        {
+            public int amount;
+            public GameObject item;
+        }
+        [SerializeField] private Loot[] _loot = null;
+
         [SerializeField] private float _throwLootForce = 0f;                       // Force that will be applied to the loot to throw it in a difference direction.
 
         public void SpreadLoot()
         {
-            foreach (GameObject loot in _lootStorage)
+            for (int lootIndex = 0; lootIndex < _loot.Length; lootIndex++)
             {
-                Rigidbody2D lootRigidbody2D = loot.GetComponent<Rigidbody2D>();
+                int itemAmount = _loot[lootIndex].amount;
 
-                if (lootRigidbody2D != null)
+                for (int itemIndex = 0; itemIndex < itemAmount; itemIndex++)
                 {
-                    lootRigidbody2D.AddForce(GetRandomDirection() * _throwLootForce, ForceMode2D.Impulse);
+                    GameObject thrownItem =
+                        Instantiate(_loot[lootIndex].item, transform.position, Quaternion.identity);
+
+                    Rigidbody2D thrownItemRigidbody2D = thrownItem.GetComponent<Rigidbody2D>();
+
+                    if (thrownItemRigidbody2D != null)
+                    {
+                        thrownItemRigidbody2D.AddForce(GetRandomDirection() * _throwLootForce, ForceMode2D.Impulse);
+                    }
                 }
             }
         }
-        
-        
+
+
         private Vector2 GetRandomDirection()
         {
             int xMin = 5, xMax = 11;
             int yMin = 4, yMax = 8;
             
-            int xDirection = Random.Range(xMin, xMax) * RandomSign();
-            int yDirection = Random.Range(yMin, yMax) * RandomSign();
+            int xDirection = UnityEngine.Random.Range(xMin, xMax) * RandomSign();
+            int yDirection = UnityEngine.Random.Range(yMin, yMax) * RandomSign();
 
             return new Vector2(xDirection, yDirection).normalized;
         }
 
         private int RandomSign()
         {
-            return Random.Range(1, 3) == 1 ? -1 : 1;
+            return UnityEngine.Random.Range(1, 3) == 1 ? -1 : 1;
         }
     }
 }
