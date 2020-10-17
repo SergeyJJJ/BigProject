169ee0256d7c39862e25813fa4221a4845eb0995
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-10-17 14:40:42 +0300

    Added boxes that can be destroyed.

---
+++ BigPoject/Assets/Scripts/Environment/ThingsDestruction/LightWeightDestruction.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using Environment.InterfacesOfUsing;
+using ForItemsAndCreatures;
+using UnityEngine;
+
+namespace Environment.ThingsDestruction
+{
+    public class LightWeightDestruction : MonoBehaviour, IBreakable
+    {
+        [SerializeField] private int _strength = 0;                        // How many times crystal can be hit before it will be broken.
+        [SerializeField] private SpriteFlash _spriteFlash = null;          // Used to do spriteFlash when entity is hitted.
+        [SerializeField] private GameObject _objectParts = null;           // Broken object parts.
+        [SerializeField] private Explosion _explosion = null;              // Used to broken object parts.  
+        
+        
+        public void Break()
+        {
+            _strength--;
+
+            if (_strength > 0)
+            {
+                OnGetDamage();
+            }
+            else
+            {
+                OnDestruction();
+            }
+        }
+        
+        
+        private void OnGetDamage()
+        {
+            if (_spriteFlash != null)
+            {
+                StartHitSpriteFlash();
+            }
+        }
+
+
+        private void OnDestruction()
+        {
+            if (_objectParts != null && _explosion != null)
+            {
+                ThrowObjectParts();
+            }
+            
+            Destroy(gameObject);
+        }
+        
+        
+        private void StartHitSpriteFlash()
+        {
+            _spriteFlash.Flash();
+        }
+        
+        
+        private void ThrowObjectParts()
+        {
+            _objectParts.transform.position = transform.position;
+            _objectParts.SetActive(true);
+            _explosion.Explode();
+        }
+    }
+}
